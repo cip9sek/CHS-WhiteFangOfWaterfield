e9d84a7d7427318941633050b36c78122f2efba9
chladma2 <chladma2@uhk.cz>
2025-03-23 22:13:57 +0100

    svg menu funs, will do the mobile first desktop later way of things

--- js/menuFunction.js
+++ js/menuFunction.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function menuFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
