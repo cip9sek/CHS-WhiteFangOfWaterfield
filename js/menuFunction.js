a6ef8b904aee737d98702f065b27b53c968dfafb
chladma2 <chladma2@uhk.cz>
2025-03-28 09:07:47 +0100

    ui

--- js/menuFunction.js
+++ js/menuFunction.js
@@ -2,5 +2,5 @@ let subMenu = document.getElementById("dropdownMenu");
 
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
-    alert("function is on");
+    //alert("function is on");
 }
